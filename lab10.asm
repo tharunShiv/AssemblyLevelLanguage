99daa2c4b7cb4946551d0197dff90b1e798a1240
tharunShiv <tharun.rocky14@gmail.com>
2018-02-27 12:55:32 +0530

    lab7 modified, output screenshot added

---
+++ lab10.asm
@@ -0,0 +1,13 @@
+; ALP to point cursor to a specifies row and column
+.model small
+
+.data
+  r db ?
+  c db ?
+  m1 db 10, 13, 'Enter row $'
+  m2 db 10, 13, 'Enter column $'
+  
+.code
+  mov ax, @data
+  mov ds, ax
+  
