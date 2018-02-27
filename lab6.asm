9b3c9fef3d031ced01b1c7d9193f33929d107191
tharunShiv <tharun.rocky14@gmail.com>
2018-02-27 11:51:41 +0530

    Lab6 program added

---
+++ lab6.asm
@@ -0,0 +1,73 @@
+.model small
+
+print macro 
+  mov dl, '\'
+  mov ah, 02h
+  int 21h
+endm
+
+.data
+  year dw ?
+  month db ?
+  date db ?
+  
+.code
+  mov ax, @data
+  mov ds, ax
+  
+  mov ax, 0600h
+  mov bh, 0edh
+  mov cx, 0000h
+  mov dx, 184fh
+  int 10h
+  
+  mov ah, 02h
+  mov bh, 00h
+  mov dl, 39d
+  mov dh, 12d
+  int 10h
+  
+  mov ah, 2ah
+  int 21h
+  
+  mov year, cx 
+  mov month, dh
+  mov date, dl
+  
+  mov al, date 
+  mov ah, 00h 
+  call display
+  print 
+  
+  mov al, month 
+  mov ah, 00h 
+  call display
+  print
+   
+  mov ax, year
+  call display 
+  mov ah, 4ch
+  int 21h 
+  
+  display proc near 
+    mov cx, 00h
+    mov bx, 10d
+    
+	
+	
+l1 : mov dx, 00h
+   div bx 
+   push dx
+   inc cx
+   cmp ax, 00h 
+   JNZ l1 
+   
+l2 : pop dx
+  add dl, 30h
+  mov ah, 02h
+  int 21h 
+  loop l2 
+  RET 
+  display endp
+  
+end
